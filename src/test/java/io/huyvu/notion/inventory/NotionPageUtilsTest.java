50a619c6e8b567988d5ba912167d5fc9858aaf76
huyvu8051 <huyvu8051@gmail.com>
2024-11-09 16:56:12 +0700

    create first unit test

---
+++ src/test/java/io/huyvu/notion/inventory/NotionPageUtilsTest.java
@@ -0,0 +1,47 @@
+package io.huyvu.notion.inventory;
+
+import notion.api.v1.model.common.Cover;
+import notion.api.v1.model.common.Icon;
+import notion.api.v1.model.common.ObjectType;
+import notion.api.v1.model.common.PropertyType;
+import notion.api.v1.model.databases.DatabaseProperty;
+import notion.api.v1.model.pages.Page;
+import notion.api.v1.model.pages.PageProperty;
+import notion.api.v1.model.pages.PagePropertyItem;
+import notion.api.v1.model.users.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NotionPageUtilsTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void getUniqueId() {
+        var properties = new HashMap<String, PageProperty>();
+
+        PageProperty.UniqueId uniqueId = new PageProperty.UniqueId();
+        uniqueId.setPrefix("TEST");
+        uniqueId.setNumber(69);
+
+
+        var value = new PageProperty("002", null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, uniqueId);
+        value.setType(PropertyType.UniqueId);
+
+
+        properties.put("title", value);
+        var page = new Page(ObjectType.Page, "001", new Icon() {}, new Cover() {
+        }, "", new User(""), "", new User(""), "", null, null, null, properties, null, null);
+        var result = NotionPageUtils.getUniqueId(page);
+        assertEquals(result, "TEST-69");
+
+    }
+}
